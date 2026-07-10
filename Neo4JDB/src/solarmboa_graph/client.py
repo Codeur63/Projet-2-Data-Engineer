30ed9af4fba015260426ad3f0b91116f33ec2acf
Codeur63 <Codeur63@gmail.com>
2026-07-10 16:10:09 +0100

    chore: modify client neo4j

--- Neo4JDB/src/solarmboa_graph/client.py
+++ Neo4JDB/src/solarmboa_graph/client.py
@@ -15,28 +15,37 @@ def get_driver():
 
     return _driver
 
+
 def get_session():
     return get_driver().session(
         database=settings.neo4j_database
     )
+    
 
 def verify_connection() -> dict:
+    
     try:
         driver = get_driver()
         driver.verify_connectivity()
+        
         return {
-            "status": "ok",
+            "statut": "connecte",
             "uri": settings.neo4j_uri,
             "user": settings.neo4j_user,
+            "version": "N/A (Driver OK)"
         }
+        
     except Exception as e:
         return {
-            "status": "error",
+            "statut": "error",
             "message": str(e),
+            "uri": settings.neo4j_uri,
+            "user": settings.neo4j_user,
+            "version": "inconnue"
         }
 
 
-def close_driver():
+def close_driver():    
     global _driver
 
     if _driver is not None:
